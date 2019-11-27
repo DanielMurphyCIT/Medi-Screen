9556fc7a649383a3c0c196b93e25e54b62c64875
ZacDair <54358391+ZacDair@users.noreply.github.com>
2019-11-27 15:47:33 +0000

    Add files via upload

---
+++ WebBaseBackend/login.php
@@ -0,0 +1,12 @@
+<html>
+	<?php
+	$email =  $_POST["email"];
+	$password =  $_POST["password"];
+	#run database connection
+	# check to see if the email is present
+	# check if password matches
+	# return boolean value
+	echo $email;
+	echo $password;
+	?>
+</html>
