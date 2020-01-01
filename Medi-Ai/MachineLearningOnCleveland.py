56458d5e3b70f656810c8468b43a742cfddc5117
ZacDair <54358391+ZacDair@users.noreply.github.com>
2020-01-01 19:41:39 +0000

    added machine learning training script
    not very accurate, 50% ish, needs save model code added

---
+++ Medi-Ai/MachineLearningOnCleveland.py
@@ -0,0 +1,40 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+
+# load the dataset
+dataset = np.loadtxt('Datasets\\Heart Disease\\processed.cleveland.data', delimiter=',', skiprows=1)
+
+# split into input (X) and output (y) variables
+
+X = dataset[:, 0:13]
+Y = dataset[:, 13]
+
+# encode class values as integers
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+
+# convert integers to dummy variables (i.e. one hot encoded)
+dummy_y = np_utils.to_categorical(encoded_Y)
+
+# define baseline model
+def baseline_model():
+    # create model
+    model = Sequential()
+    model.add(Dense(8, input_dim=13, activation='relu'))
+    model.add(Dense(5, activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5)
+kfold = KFold(n_splits=10, shuffle=True)
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean() * 100, results.std() * 100))
